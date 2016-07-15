d565bba1b5bc0617b79a1ec37a27b859d47fa70e
Zhenfei Nie <youyis2fox@gmail.com>
2016-07-15 10:51:23 +0200

    v2

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var http = require('http');
+var fs = require('fs');
+var debug = require('./debug.js').debug;
+var debugNL = require('./debug.js').debugNL;
 
 photoid = 2380756;
 
@@ -66,27 +69,69 @@ var listOptions = {
     }
 };   
 
+var imgOptions = {
+    hostname: 'img.plantphoto.cn',
+    port: 80,
+    path: '/image2/b/2380756.jpg',
+    method: 'GET',
+    headers: {
+        'Host': 'img.plantphoto.cn',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36', 
+        'Referer': 'http://www.plantphoto.cn/tu/2380756', 
+        'Accept': 'image/webp,image/*,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Cookie': 'AJSTAT_ok_pages=2; AJSTAT_ok_times=1',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    }
+};
+
 http.get(nameOptions, function(res) {
-    console.log("\nName: " + res.statusCode);
+    debugNL("\nName: " + res.statusCode);
     res.on("data", function(chunk) {
-        console.log("   name: " + chunk);
+        debugNL("   name: " + chunk);
     });
 });
 
 http.get(placeOptions, function(res) {
-    console.log("\nPlace: " + res.statusCode);
+    debugNL("\nPlace: " + res.statusCode);
     res.on("data", function(chunk) {
-        console.log("   place: " + chunk);
+        debugNL("   place: " + chunk);
     });
 });
 
 http.get(listOptions, function(res) {
-    console.log("\nList: " + res.statusCode);
+    debugNL("\nList: " + res.statusCode);
     res.on("data", function(chunk) {
-        console.log("   list: " + chunk);
+        debugNL("   list: " + chunk);
     });
 });
 
+debugNL(' -- Start fetching image ' + photoid + ' -- ');
+http.get(imgOptions, function(res) {
+    debugNL('SYN', res.statusCode, res.statusMessage);
+
+    var data = new Buffer('');
+    res.on('data', function(chunk) {
+        debug(chunk.length + ' ');
+        var length = data.length + chunk.length;
+        data = Buffer.concat([data, chunk], length);
+    });
+
+    res.on('end', function() {
+        debugNL('\nFinish: image size', data.length);
+        fs.writeFile(photoid + '.png', data, function(err) {
+            if (err) {
+                debugNL('Failed to get ' + photoid);
+            } else {
+                debugNL('Fetch image' + photoid + ' successfully\n');
+            }
+        });
+    });
+});  
 
 
   
